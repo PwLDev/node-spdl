e1f1761aa54fc27f51def903a7a98f139ba51cef
PwLDev <pgibran09@gmail.com>
2025-04-25 20:31:26 -0700

    update proto path in loader

--- src/lib/proto.ts
+++ src/lib/proto.ts
@@ -1,6 +1,5 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-
 import proto from "protobufjs";
 
 export const resolveProto = async (
@@ -9,7 +8,7 @@ export const resolveProto = async (
     message: string
 ): Promise<any> => {
     const root: proto.Root = await proto.load(
-        path.resolve(`./src/proto/${file}.proto`)
+        path.resolve(`./proto/${file}.proto`)
     );
 
     const messageType = root.lookupType(message);
@@ -24,13 +23,13 @@ export const resolveProto = async (
 
 const resolveProtoPath = (filename: string): string => {
 	if (typeof __dirname !== "undefined") {
-		return path.join(__dirname, "..", "/proto", filename);
+		return path.join(__dirname, "../../proto", filename);
 	// @ts-ignore
 	} else if (typeof import.meta !== "undefined" && import.meta.url) {
 		// @ts-ignore
 		const __filename = fileURLToPath(import.meta.url);
 		const __dirname = path.dirname(__filename);
-		return path.join(__dirname, "..", "/proto", filename);
+		return path.join(__dirname, "../../proto", filename);
 	} else {
 		throw new Error("Unable to resolve proto path. Unsupported module environment.");
 	}
